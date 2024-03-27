8ec65cd8355b392c3190ae43997e8d4fd8a51dc0
gnunez0101 <116986696+gnunez0101@users.noreply.github.com>
2024-03-26 19:10:57 -0500

    Animations fixed

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import './Task.css'
 
 function Task({task} : {task: any}) {
@@ -6,12 +7,16 @@ function Task({task} : {task: any}) {
   const countCompleted = task.subtasks.filter((c: any) => c.isCompleted).length
 
   return (
-    <section className="task">
+    <motion.section className="task"
+      initial = {{ scale: 1}}
+      whileHover = {{ scale: [1.05, 1, 1.02], transition: {duration: 0.5} }}
+      whileTap   = {{ scale: 0.98 }}
+    >
       <div className="task-title">{task.title}</div>
       <div className="subtasks">
         {`${countCompleted} of ${countSubTasks} subtasks`}
       </div>
-    </section>
+    </motion.section>
   );
 }
 export default Task;
