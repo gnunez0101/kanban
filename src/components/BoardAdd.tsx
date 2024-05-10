cfb9856fec2d071bdd76958d2e5e5a1f8b0a8a66
gnunez0101 <116986696+gnunez0101@users.noreply.github.com>
2024-05-09 23:28:47 -0500

    BoardAdd buttons +Add New Column and X for Delete work with animation and unique keys.

--- src/components/BoardAdd.tsx
+++ src/components/BoardAdd.tsx
@@ -1,18 +1,28 @@
 import './BoardAdd.css'
 import DialogModal   from "./DialogModal";
 import { Button } from "./Button";
-import { useEffect, useState } from "react";
+import { useEffect, useId, useState } from "react";
 import useDatabase from '../hooks/useDatabase';
 import useDialogs  from "../hooks/useDialogs";
+import { AnimatePresence, motion } from 'framer-motion';
 
+type typeColumns = {
+  id: string,
+  name: string
+}
 
 export function BoardAdd ( { edit = false }: { edit?: boolean } ) {
   const [columns, setColumns] = useState(null)
   const { database } = useDatabase()
   const { dialogLaunch, dialogsData } = useDialogs()
   const [boardName, setBoardName] = useState("")
-  const [tempColumns, setTempColumns] = useState<string[]>([])
-  const defaultColumns = ["Todo", "Doing"]
+  const [counter, setCounter] = useState(0)
+  
+  const [tempColumns, setTempColumns] = useState<typeColumns[]>([])
+  const defaultColumns = [
+    {"id": "00", "name": "Todo" }, 
+    {"id": "11", "name": "Doing"}
+  ]
 
   let firstTime = true
   useEffect(() => {
@@ -22,7 +32,9 @@ export function BoardAdd ( { edit = false }: { edit?: boolean } ) {
         const board = dialogsData[1]
         setBoardName(database.boards[board].name)
         if (database.boards[board].columns.length > 0)
-          setTempColumns(database.boards[board].columns.map(column => column.name))
+          setTempColumns(database.boards[board].columns.map(column => {
+            return( {id: useId(), name: column.name} )
+          }))
         else
           setTempColumns(defaultColumns)
       }
@@ -34,6 +46,19 @@ export function BoardAdd ( { edit = false }: { edit?: boolean } ) {
 
   function closeDialog() {
     dialogLaunch("close", dialogsData[1], 0, 0)
+  }
+
+  
+  function addColumn() {
+    setTempColumns([...tempColumns, 
+      {id: counter.toString(), name: `New column ${tempColumns.length+1}...`}])
+    setCounter(counter + 1)
+  }
+
+  function deleteColumn(index: number) {
+    const _tempColumns = tempColumns
+    _tempColumns.splice(index, 1)
+    setTempColumns([..._tempColumns])
   }
   
   return (
@@ -52,11 +77,30 @@ export function BoardAdd ( { edit = false }: { edit?: boolean } ) {
       <section className="boardadd__columns">
         <div className="boardadd__columns--title">Board Columns</div>
         <div className="boardadd__columns--items">
-          { tempColumns.map((column: string, index: number) => 
-            <Column name={column} key={index} />
-          )}
-          <Button className="boardadd__btn-addcol secondary">
-            + Add New Column
+          <AnimatePresence mode='popLayout'>
+            { tempColumns.length ? (
+              tempColumns.map((column: typeColumns, index: number) => 
+                <motion.div className='boardadd__column--body' key={column.id}
+                  layout
+                  initial={{ opacity: 0, x: -400, scale: 0.5 }}
+                  animate={{ opacity: 1, x: 0, scale: 1 }}
+                  exit={{ opacity: 0, x: 200, scale: 1.2 }}
+                  transition={{ duration: 0.6, type: "spring" }}
+                >
+                  <input className="boardadd__column--name" type="text" 
+                    defaultValue={column.name}
+                  />
+                  <div   className="boardadd__column--delete"
+                    onClick={() => deleteColumn(index)}
+                  />
+                </motion.div>
+              )) : <div><h1 className='no-columns'>No columns!</h1></div>
+            } 
+
+          </AnimatePresence>
+          <Button className="boardadd__btn-addcol secondary"
+            onClick={addColumn}
+          >+ Add New Column
           </Button>
         </div>
       </section>
@@ -65,15 +109,5 @@ export function BoardAdd ( { edit = false }: { edit?: boolean } ) {
         Create New Board
       </Button>
     </DialogModal>
-  )
-}
-
-function Column( { name }: { name: string } ) {
-  const [columnInput, setColumnInput] = useState("")
-  return (
-    <div className='boardadd__column--body'>
-      <input className="boardadd__column--name" type="text" defaultValue={name}/>
-      <div   className="boardadd__column--delete"></div>
-    </div>
   )
 }
